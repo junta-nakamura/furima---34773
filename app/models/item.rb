67932a747810ae9b4b6888699dfd7f1b0bd4da49
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-04 19:14:08 +0900

    itemモデル　バリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  validates :product_name,    presence: true
+  validates :detail,          presence: true
+  validates :category_id,     presence: true
+  validates :status_id,       presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefectures_id,  presence: true
+  validates :days_id,         presence: true
+  validates :price,           presence: true
 end

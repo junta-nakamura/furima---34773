2e1401b9aa5ed3b988870325fb7c0ad94ebaaa47
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-04 19:37:12 +0900

    itemモデル　バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,7 @@ class Item < ApplicationRecord
   validates :delivery_fee_id, presence: true
   validates :prefectures_id,  presence: true
   validates :days_id,         presence: true
-  validates :price,           presence: true
+  validates :price,           presence: true,
+                              numericality: greater_than: 300,
+                              numericality: less_than_or_equal_to: 9999999
 end

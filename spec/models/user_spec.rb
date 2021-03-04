6e80ed0a27dce608ac22dbef2f2e44117aa784a1
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-04 14:46:16 +0900

    テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -50,6 +50,24 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
 
+    it 'passwordは半角英語のみでは登録できない' do
+      @user.password = "aaaaaa"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+
+    it 'passwordは半角数字のみでは登録できない' do
+      @user.password = "111111"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+
+    it 'passwordは全角では登録できない' do
+      @user.password = "ａａａＡＡＡ"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+
     it 'password_confirmationが空では登録できない' do
       @user.password_confirmation = ""
       @user.valid?

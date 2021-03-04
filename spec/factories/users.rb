b9e779be25480c19765f45bc66d4fdb419f39daf
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-04 14:58:21 +0900

    passwordのFakerの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name_kanji       {'中村'}
     first_name_kanji      {'ジュんた'}

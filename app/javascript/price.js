f35ebe0022644b5b59c9058e54fa7ed94db45159
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-05 17:27:02 +0900

    出品画面にて価格の入力と同時に手数料、販売利益の計算がされる

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,8 +3,9 @@ function setting() {
   itemPrice.addEventListener("input", () => {
     const countVal = itemPrice.value;
     const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = parseInt(countVal * 0.1);
     const profit = document.getElementById("profit");
+    addTaxPrice.innerHTML = parseInt(countVal * 0.1);
+    addTaxPrice.innerHTML.toLocaleString();
     profit.innerHTML = countVal - addTaxPrice.innerHTML
   });
 }

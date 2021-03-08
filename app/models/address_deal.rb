f9f4795f2c018f59443de9b2d5ff3bab936b93ae
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-08 15:36:21 +0900

    Formオブジェクト内の改行、位置変更

--- app/models/address_deal.rb
+++ app/models/address_deal.rb
@@ -1,13 +1,15 @@
 class AddressDeal
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number, :building, :phone, :deal_id, :user_id, :item_id 
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :house_number,
+                :building, :phone, :deal_id, :user_id, :item_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code
     validates :prefecture_id
     validates :municipalities
     validates :house_number
-    validates :phone  
+    validates :phone
     validates :user_id
     validates :item_id
   end
@@ -15,7 +17,7 @@ class AddressDeal
   POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
   validates_format_of :postal_code, with: POSTAL_CODE_REGEX, message: "Input correctly"
   
-  PHONE_REGEX = /\A\d{11}\z/
+  PHONE_REGEX = /\A\d{10,11}\z/
   validates_format_of :phone, with: PHONE_REGEX, message: "Input only number"
 
   def save
@@ -23,5 +25,5 @@ class AddressDeal
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities,
                    house_number: house_number, building: building, phone: phone, deal_id: deal.id)
   end
-  
+
 end

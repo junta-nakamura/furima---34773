b2eced4b843d44193a0280ad5c0e44f380aae477
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-08 12:07:36 +0900

    PAY.JPの道入、カード決済機能実装

---
+++ app/javascript/card.js
@@ -0,0 +1,33 @@
+const pay = ()=> {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+    const card = {
+      number: formData.get("address_deal[number]"),
+      cvc: formData.get("address_deal[cvc]"),
+      exp_month: formData.get("address_deal[exp_month]"),
+      exp_year: `20${formData.get("address_deal[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    })
+  })
+}
+
+window.addEventListener("load", pay);

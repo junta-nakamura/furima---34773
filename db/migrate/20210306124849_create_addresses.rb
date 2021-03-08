db1493a699369fb24d15baed4dc387064bf60034
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-08 22:47:57 +0900

    Update 20210306124849_create_addresses.rb

--- db/migrate/20210306124849_create_addresses.rb
+++ db/migrate/20210306124849_create_addresses.rb
@@ -7,9 +7,9 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :house_number,   null: false
       t.string     :building
       t.string     :phone,          null: false
-      t.references :deal,           foreign_key: true
+      # t.references :deal,           foreign_key: true
       t.timestamps
-      
+
     end
   end
 end

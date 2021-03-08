6e4fe5208b91fa0caf101f1b6c1a30fc3a5255e1
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-08 12:12:00 +0900

    自身の商品に対する購入画面への遷移、売切商品の購入画面への遷移対策

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,8 +1,13 @@
 class DealsController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_root
 
   def index
     @address_deal = AddressDeal.new
+    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+     redirect_to root_path
+    end
   end
 
   def create
@@ -21,4 +26,10 @@ class DealsController < ApplicationController
     params.require(:address_deal).permit(:postal_code, :prefecture_id, :municipalities, :house_number, :building, :phone, :deal_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def move_to_root
+    @item = Item.find(params[:item_id])
+    if !@item.deal.blank?
+      redirect_to root_path
+    end
+  end
 end

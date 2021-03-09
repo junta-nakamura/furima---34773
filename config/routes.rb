44d6368b0813c4f09a39f10f2844e06c7f23626f
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-09 11:54:44 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :items do
     resources :deals, only: [:index, :create]
   end
   
